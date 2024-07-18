5b2113e00ee3a593b8b3ac6407771c05c8c5ff6b
Kokoro <farmteanotes@gmail.com>
2024-07-18 10:29:18 +0900

    初回コミット

---
+++ StudentManagement/src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -0,0 +1,21 @@
+package raisetech.StudentManagement;
+
+import java.awt.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+	};
+
+	@GetMapping("/diary")
+	public String hello(){
+		return "今日は雨でした。" ;
+	}
+}
