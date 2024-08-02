39bc71352d68cb9303a1bec5f12c086976a33da1
Kokoro <farmteanotes@gmail.com>
2024-08-02 12:26:59 +0900

    23_MyBatisを使ってCRUDのRead処理を実装 StudentCourseの全件取得

--- StudentManagement/src/main/java/raisetech/StudentManagement/Student.java
+++ StudentManagement/src/main/java/raisetech/StudentManagement/Student.java
@@ -1,26 +1,20 @@
 package raisetech.StudentManagement;
 
-import org.apache.ibatis.annotations.Update;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Student {
 
-  private String name;
+  private String id;
+  private String fullName;
+  private String nameRuby;
+  private String nickname;
+  private String emailAddress;
+  private String address;
   private int age;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
+  private String gender;
 
 }
+
--- StudentManagement/src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ StudentManagement/src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,17 +1,11 @@
 package raisetech.StudentManagement;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -25,74 +19,13 @@ public class StudentManagementApplication {
     SpringApplication.run(StudentManagementApplication.class, args);
   }
 
-  @GetMapping("/student")
-  public String getStudent(@RequestParam("name") String name) {
-    Student student = repository.searchByName(name);
-    return student.getName() + " " + student.getAge() + "歳";
-  }
-
   @GetMapping(value = "/studentList", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public List<String> getStudentList() {
-    List<Student> students = repository.searchAll();
-    List<String> studentList = students.stream()
-        .map(student -> student.getName() + " " + student.getAge() + "歳")
-        .collect(Collectors.toList());//要素をリストに収集
-    /*List<String> studentList = new ArrayList<>(); 　//リストを生成
-    for (Student student : students) {
-      String studentInfo = student.getName() + " " + student.getAge() + "歳";　//studentInfoに情報を取得して入れる
-      studentList.add(studentInfo);  //リストに加える
-      //リストに情報がなくなるまで繰り返す
-      */
-
-    return studentList;
+  public List<Student> getStudentList() {
+    return repository.search();
   }
 
-  @PostMapping("/student")
-  public void registerStudent(String name, int age) {
-    repository.registerStudent(name, age);
+  @GetMapping(value = "/studentCourseList", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public List<StudentCourses> getStudentCourseList() {
+    return repository.searchStudentCourses();
   }
-
-  @PatchMapping("/student")
-  public void updateStudent(String name, int age) {
-    repository.updateStudent(name, age);
-  }
-
-  @DeleteMapping("/student")
-  public void deleteStudent(String name) {
-    repository.deleteStudent(name);
-  }
-
-
-/*
-  private Map<String, String> student = new HashMap<>();//Mapの初期化
-
-  public static void main(String[] args) {
-    SpringApplication.run(StudentManagementApplication.class, args);
-  }
-
-  @GetMapping("/student")
-  public Map<String, String> getStudent() {
-    return student;
-  }
-
-  @PostMapping("/student")
-  public void setStudent(String name, String age) {
-    student.put(name, age);
-  }
-
-  @PostMapping("/updateName")
-  public void updateStudentName(String oldName, String newName) {
-    if (student.containsKey(oldName)) {
-      String age = student.remove(oldName);//古い名前を削除して、ageのみを取得＆格納
-      student.put(newName, age);//新しい名前に変更
-    }
-  }
-
-  @PostMapping("/updateAge")
-  public void updateStudentAge(String name, String newAge) {
-    if (student.containsKey(name)) {//名前が同じ時に年齢のみ書き換え
-      student.put(name, newAge);
-    }
-  }*/
-
 }
