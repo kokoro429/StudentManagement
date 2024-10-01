f733e97ee5686b9c8aff6acaae96f06f1e845bc3
Kokoro <farmteanotes@gmail.com>
2024-10-01 20:55:32 +0900

    36_SpringBootでの例外処理 change:エラーメッセージがJSON形式で表示されるように変更

--- StudentManagement/src/main/java/raisetech/StudentManagement/controller/handler/Handler.java
+++ StudentManagement/src/main/java/raisetech/StudentManagement/controller/handler/Handler.java
@@ -1,5 +1,7 @@
 package raisetech.StudentManagement.controller.handler;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,13 +12,18 @@ import raisetech.StudentManagement.exception.TestException;
 public class Handler {
 
   @ExceptionHandler(TestException.class)
-  public ResponseEntity<String> handleTestException(TestException ex) {
-    return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+  public ResponseEntity<Map<String, String>> handleTestException(TestException ex) {
+    Map<String, String> errorResponse = new HashMap<>();
+    errorResponse.put("message", ex.getMessage()); // "message"キーでエラーメッセージを設定
+    // MapをResponseEntityで返す
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
   }
 
   @ExceptionHandler(IllegalArgumentException.class)
-  public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("IDが無効です:" + ex.getMessage());
+  public ResponseEntity<Map<String, String>> handleIllegalArgumentException(
+      IllegalArgumentException ex) {
+    Map<String, String> errorResponse = new HashMap<>();
+    errorResponse.put("message", ex.getMessage());
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
   }
-
 }
