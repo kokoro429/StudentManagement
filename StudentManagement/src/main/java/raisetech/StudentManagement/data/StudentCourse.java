ae98c19aef1d2ae5cd91bcecfb65214c8b7decd7
Kokoro <farmteanotes@gmail.com>
2024-10-02 18:12:42 +0900

    38_ドキュメントの必要性と作り方 講義内の実装 OpenAIをインストールし、使えるようにする

--- StudentManagement/src/main/java/raisetech/StudentManagement/data/StudentCourse.java
+++ StudentManagement/src/main/java/raisetech/StudentManagement/data/StudentCourse.java
@@ -1,5 +1,6 @@
 package raisetech.StudentManagement.data;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import lombok.Getter;
@@ -9,6 +10,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 /**
  * 受講生コース情報を扱うオブジェクト
  */
+@Schema(description = "受講生コース情報")
 @Getter
 @Setter
 public class StudentCourse {
